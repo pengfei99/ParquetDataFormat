61d36220f35b5bd87e52036f85826d9614206da8
pengfei <liu.pengfei@hotmail.fr>
2021-09-22 08:17:29 +0000

    update

--- src/bash/s3_download.sh
+++ src/bash/s3_download.sh
@@ -4,6 +4,6 @@ bucket_name=ursa-labs-taxi-data
 
 for i in 01 02 03 04 05 06 07 08 09 10 11 12
 do
-  aws s3api get-object --bucket ${bucket_name} --key 2010/${i}/data.parquet 2010_${i}_data.parquet  --no-sign-request
+  aws s3api get-object --bucket ${bucket_name} --key 2012/${i}/data.parquet 2012_${i}_data.parquet  --no-sign-request
 done
 
--- src/bash/upload_minio.sh
+++ src/bash/upload_minio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-minio_path=s3/pengfei/diffusion/data_format/ny_taxis/parquet/raw_bis/
+minio_path=s3/pengfei/diffusion/data_format/ny_taxis/parquet/raw_2011_2012/
 
 for i in 01 02 03 04 05 06 07 08 09 10 11 12
 do
-   mc cp 2010_${i}_data.parquet ${minio_path}
+   mc cp 2012_${i}_data.parquet ${minio_path}
 done
